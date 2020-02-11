c85c7965a51a1cf3e00617054a9070a3d355c26c
shogo takayama <shogo@chalkboard.me>
2020-02-11 21:47:25 +0900

    IF/四角形オブジェクトの生成

--- src/Main2.java
+++ src/Main2.java
@@ -1,21 +1,28 @@
 import shape2.Shape;
+import shape2.Square;
 import shape2.Tryangle;
 
 public class Main2 {
-    public static void main(String[] args){
-      learn1();
-    }
+  public static void main(String[] args){
+    learn1();
+    learn2();
+  }
 
-    private static void learn1() {
-      // オブジェクトの作成
-      Tryangle tryangle = new Tryangle("shape1-tryangle", 7, 6, 5);
-      print(tryangle);
-    }
+  private static void learn1() {
+    // オブジェクトの作成
+    Tryangle tryangle = new Tryangle("shape2-tryangle", 7, 6, 5);
+    print(tryangle);
+  }
 
-    private static void print(Shape shape) {
-      System.out.println(
-          // %sは文字列を引数に取る。 %.4fは浮動小数点を引数にとり、.4fは小数点第四位で四捨五入を行う。
-          String.format("%s: 面積->%.4f", shape.getName(), shape.area())
-      );
-    }
+  private static void learn2() {
+    Square square = new Square("shape2-square", 4, 5, 4, 5);
+    print(square);
+  }
+
+  private static void print(Shape shape) {
+    System.out.println(
+        // %sは文字列を引数に取る。 %.4fは浮動小数点を引数にとり、.4fは小数点第四位で四捨五入を行う。
+        String.format("%s: 面積->%.4f", shape.getName(), shape.area())
+    );
+  }
 }
---
+++ src/shape2/Square.java
@@ -0,0 +1,42 @@
+package shape2;
+
+/**
+ * 四角形オブジェクト
+ */
+public class Square implements Shape{
+  // オブジェクト生成後、一切替わることのないフィールドであるならば、finalをつけることで明示できる
+  private final String name;
+
+  private final Integer edgeAB;
+
+  private final Integer edgeBC;
+
+  private final Integer edgeCD;
+
+  private final Integer edgeAD;
+
+  public Square(String name, Integer edgeAB, Integer edgeBC, Integer edgeCD, Integer edgeAD) {
+    this.name = name;
+    this.edgeAB = edgeAB;
+    this.edgeBC = edgeBC;
+    this.edgeCD = edgeCD;
+    this.edgeAD = edgeAD;
+  }
+
+  @Override
+  public Double area(){
+    return brahmaguptasFormula(edgeAB, edgeBC, edgeCD, edgeAD);
+  }
+
+  @Override
+  public String getName(){
+    return this.name;
+  }
+
+  // 四角形の辺から面積を求めるブラーマグプタの公式
+  private Double brahmaguptasFormula(Integer ab, Integer bc, Integer cd, Integer ad) {
+    Double s = (ab + bc + cd + ad) / 2.0;
+    Double S = (s - ab) * (s - bc) * (s - cd) * (s - ad);
+    return Math.sqrt(S);
+  }
+}
